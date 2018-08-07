7bdc6da800b1941910b9e54ccf21682694cf14a2
Tricia Leavitt <leavitt.tricia@gmail.com>
2018-08-06 20:31:22 -0700

    Added source

--- Inheritance_Patterns/Prototypal_inheritance.js
+++ Inheritance_Patterns/Prototypal_inheritance.js
@@ -5,6 +5,8 @@ Typically use Object.create()
 Delegation
 Concatenative
 Functional 
+
+https://medium.com/javascript-scene/master-the-javascript-interview-what-s-the-difference-between-class-prototypal-inheritance-e4cd0a7562e9 
 */ 
 
 const person = {
