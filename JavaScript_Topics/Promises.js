bd5c5baaacb747f76b6ecbed9a91c0ae6ec3c025
Tricia Leavitt <leavitt.tricia@gmail.com>
2018-08-06 21:40:42 -0700

    Added Promises (ES6)

---
+++ JavaScript_Topics/Promises.js
@@ -0,0 +1,26 @@
+/* A promise is an object that represents a single value some time in the future
+based on JavaScript's asynchronous nature
+Promises are a returned object to which you attach callbacks, 
+instead of passing callbacks into a function.
+Promises are immutible (cannot be changed) and must return a value
+Promises have three states: fulfilled, pending, and rejected
+Promises must contain a .then() method
+Add a .catch() method to the end 
+
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Using_promises
+https://codeburst.io/the-es6-promises-87a979ab27e4
+*/
+
+const isSmallThenTen = (num) => {
+    return new Promise((resolve, reject) => {
+        if(num < 10) {
+            resolve(true)
+        } else {
+            reject(false)
+        }
+    })
+}
+
+isSmallThenTen(9)
+    .then(res => console.log('The number is smaller then 10'))
+    .catch(err => console.log('The number is not smaller then 10'))
