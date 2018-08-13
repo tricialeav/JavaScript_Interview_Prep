903a1619650a495f683ffafb1f486c093bafcc5f
Tricia Leavitt <leavitt.tricia@gmail.com>
2018-08-13 16:34:39 -0700

    Added array.concat() reference

---
+++ JavaScript_Reference/Array_Concat.js
@@ -0,0 +1,8 @@
+/* array.concat() can be used to merge two or more arrays into 
+one new array */
+
+const array1 = [1, 2, 3, 4, 5]; 
+const array2 = [6, 7, 8, 9, 10]; 
+const newArray = array1.concat(array2); 
+
+console.log(newArray); // [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]; 
